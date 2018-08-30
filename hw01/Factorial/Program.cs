c19809a9e721837eb833da5a2009b03f3f7312b9
Supanat <supanat.ji@gmail.com>
2018-08-30 14:33:29 +0700

    hw01

---
+++ hw01/Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial");
+            Console.WriteLine("Enter the number :");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+
+            /* 
+            for(int i=number ; i>=1; i--)
+            {
+                result *= i;
+            }
+            */
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine("Factorial of {0} is {1}",number,result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hw01/MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication Table");
+            Console.WriteLine("Enter the number :");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for(int i =1;i<=100;i++){
+                Console.WriteLine("{0} x {1} = {2}",number,i,number*i);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
